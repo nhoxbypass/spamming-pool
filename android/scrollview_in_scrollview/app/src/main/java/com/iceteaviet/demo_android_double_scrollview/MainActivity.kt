54cf1f4c203ebe3c96f8956b52b5e9833f244a43
nhoxbypass <12061934+nhoxbypass@users.noreply.github.com>
2019-03-03 15:27:25 +0700

    Add scrollview in scrollview example

---
+++ android/scrollview_in_scrollview/app/src/main/java/com/iceteaviet/demo_android_double_scrollview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.iceteaviet.demo_android_double_scrollview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ android/scrollview_in_scrollview/app/src/main/java/com/iceteaviet/demo_android_double_scrollview/MyFrameLayout.kt
@@ -0,0 +1,18 @@
+package com.iceteaviet.demo_android_double_scrollview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.FrameLayout
+
+class MyFrameLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : FrameLayout(context, attrs, defStyle) {
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        parent.requestDisallowInterceptTouchEvent(true)
+        return super.onInterceptTouchEvent(ev)
+    }
+}
