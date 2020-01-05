e0bf50e4744fa7f4f3bd9e17c1cd56e0e6491cca
tamdh2 <tamdh2@vng.com.vn>
2020-01-05 18:28:26 +0700

    Add Android schedulers

---
+++ android/schedulers/app/src/main/java/com/iceteaviet/schedulers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iceteaviet.schedulers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
