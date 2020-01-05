75d315e1e494c08d386adf0ce30753e261bab086
tamdh2 <tamdh2@vng.com.vn>
2020-01-05 23:59:42 +0700

    Add executor android schedulers

---
+++ android/schedulers/app/src/main/java/com/iceteaviet/schedulers/ExecutorAndroidSchedulers.kt
@@ -0,0 +1,84 @@
+package com.iceteaviet.schedulers
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
+
+/**
+ * Android schedulers using [java.util.concurrent.Executor]
+ */
+class ExecutorAndroidSchedulers private constructor(
+        /**
+         * Schedulers
+         */
+        private val handler: Handler) {
+    private val scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
+    private val emissions = ConcurrentHashMap<Any, Future<*>?>()
+    /**
+     * Debounce {@param runnable} by {@param delay}
+     * Schedules it to be executed after given delay, or cancels its execution
+     * if the method is called with the same key again within the delay time.
+     */
+    fun debounce(key: Any, runnable: Runnable, delay: Long, unit: TimeUnit) {
+        val prev = emissions.put(key, scheduledExecutor.schedule({
+            try {
+                handler.post(runnable)
+            } finally {
+                emissions.remove(key)
+            }
+        }, delay, unit))
+        prev?.cancel(true)
+    }
+
+    /**
+     * Debounce {@param runnable} by {@param delay}
+     * Schedules it to be executed after given delay, and cancels other schedules
+     * if the method is called with the same key again within the delay time.
+     */
+    fun throttleFirst(key: Any, runnable: Runnable, delay: Long, unit: TimeUnit) {
+        if (emissions.containsKey(key)) return
+        emissions[key] = scheduledExecutor.schedule({
+            try {
+                handler.post(runnable)
+            } finally {
+                emissions.remove(key)
+            }
+        }, delay, unit)
+    }
+
+    fun shutdown() {
+        scheduledExecutor.shutdownNow()
+    }
+
+    /**
+     * Key to determine the group ID of debounce / throttle
+     */
+    object Key {
+        const val TEST_KEY = "TEST_KEY"
+    }
+
+    companion object {
+        private val LOCK = Any()
+        private var T_MAIN_INSTANCE: ExecutorAndroidSchedulers? = null
+        /**
+         * A scheduler which executes actions on the Android main thread.
+         *
+         *
+         * The returned scheduler will post asynchronous messages to the looper by default.
+         */
+        fun mainThread(): ExecutorAndroidSchedulers {
+            if (T_MAIN_INSTANCE == null) {
+                synchronized(LOCK) {
+                    if (T_MAIN_INSTANCE == null) {
+                        T_MAIN_INSTANCE = ExecutorAndroidSchedulers(Handler(Looper.getMainLooper()))
+                    }
+                }
+            }
+            return T_MAIN_INSTANCE!!
+        }
+    }
+
+}
---
+++ android/schedulers/app/src/main/java/com/iceteaviet/schedulers/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.iceteaviet.schedulers
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvCount: TextView
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvCount = findViewById(R.id.tv_count)
+        findViewById<Button>(R.id.btn_tap).setOnClickListener {
+            Log.d("Genius", "onClick()")
+            // Update color on each click
+            tvCount.setTextColor(genRandomColor())
+
+            ExecutorAndroidSchedulers.mainThread().debounce(ExecutorAndroidSchedulers.Key.TEST_KEY, Runnable {
+                Log.d("Genius", "On result debounce!")
+
+                count++
+                tvCount.text = count.toString()
+            }, 3, TimeUnit.SECONDS)
+        }
+    }
+
+    private fun genRandomColor(): Int {
+        val rnd = Random()
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+    }
+}
